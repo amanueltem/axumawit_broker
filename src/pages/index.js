b4acf73376d92abcbe6b8b52eca8a10bb2545cd1
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-11-08 15:51:09 +0300

    Stop tracking src directory

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./home/Home";
-export { default as Contact } from "./contact/Contact";
-
-export { default as Admin } from "./admin/Admin";
-export {default as Login} from "./auth/Login"
